6a2d55a7e68e78183abb87cf1dd78fb813db2d1c
josemg08 <josemg08@gmail.com>
2015-10-30 12:37:23 -0300

    First version working.

--- app/src/main/java/com/example/jose_gonzalez/udacityportfolio/PortfolioActivity.java
+++ app/src/main/java/com/example/jose_gonzalez/udacityportfolio/PortfolioActivity.java
@@ -3,28 +3,35 @@ package com.example.jose_gonzalez.udacityportfolio;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 
-/**.___
+/**
+ * .___
  * Created by Jose Gonzalez, 28/10/2015
- __.*/
+ * __.
+ */
 @EActivity(resName = "portfolio_activity")
 public class PortfolioActivity extends AppCompatActivity {
 
     @ViewById(resName = "toolbar")
     protected Toolbar mToolbar;
-    @ViewById(resName = "description_text")
-    protected TextView mDescriptionText;
     @ViewById(resName = "list_view")
     protected ListView mListView;
+    @ViewById(resName = "description_text")
+    protected TextView mDescriptionText;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -32,17 +39,26 @@ public class PortfolioActivity extends AppCompatActivity {
     }
 
     @AfterViews
-    protected void init(){
+    protected void init() {
         mToolbar.setTitle(R.string.my_udacity_portfolio);
         setSupportActionBar(mToolbar);
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
-                this,
-                R.layout.item_primary_button,
-                getResources().getStringArray(R.array.nonodegree_apps_array) );
+        PortfolioButtonAdapter adapter = new PortfolioButtonAdapter();
+        adapter.items = getResources().getStringArray(R.array.nonodegree_apps_array);
+        mListView.setAdapter(adapter);
 
-        mListView.setAdapter(arrayAdapter);
+        mDescriptionText.setText(R.string.my_nanodegree_apps);
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        /*mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(), "App", Toast.LENGTH_SHORT).show();
+            }
+        });*/
     }
 
     @Override
@@ -65,6 +81,42 @@ public class PortfolioActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class PortfolioButtonAdapter extends BaseAdapter{
+
+        private String[] items;
+
+        @Override
+        public int getCount() {
+            return items.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return items[i];
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(final int i, View view, ViewGroup viewGroup) {
+            view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_primary_button,null);
+            ((Button)view.findViewById(R.id.button)).setText(items[i]);
+
+            final int element = i;
+            view.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(view.getContext(), " " + items[element], Toast.LENGTH_LONG).show();
+                }
+            });
+
+            return view ;
+        }
     }
 
 }
--- app/src/main/java/com/example/jose_gonzalez/udacityportfolio/PortfolioFragment.java
+++ app/src/main/java/com/example/jose_gonzalez/udacityportfolio/PortfolioFragment.java
@@ -1,17 +1,22 @@
 package com.example.jose_gonzalez.udacityportfolio;
 
 import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-import org.androidannotations.annotations.EFragment;
-
-/**
- * .___
+/**.___
  * Created by Jose Gonzalez, 28/10/2015
- * <p/>
+ *
  * A placeholder fragment containing a simple view.
- * __.
- */
-@EFragment(resName = "portfolio_fragment_container")
+ __.*/
 public class PortfolioFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.portfolio_fragment, container, false);
+    }
 
 }
