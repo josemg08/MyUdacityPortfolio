504afff49039dee07b9f4e25bd64676830f2e25e
josemg08 <josemg08@gmail.com>
2016-01-15 12:53:00 -0300

    Starting add the youtubePlayerView.

---
+++ popularmovies/src/main/java/com/example/jose_gonzalez/popularmovies/Config.java
@@ -0,0 +1,10 @@
+package com.example.jose_gonzalez.popularmovies;
+
+/**.___
+ * Created by jose-gonzalez on 15/01/16.
+ __.*/
+public final class Config {
+
+    public static final String YOUTUBE_API_KEY = "popularmovies-1189";
+
+}
